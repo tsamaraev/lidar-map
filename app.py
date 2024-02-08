d1c111d09a7ce78d5a7a75254ba061d6c702dad0
user9925 <tsamaraevv@mail.ru>
2024-02-08 09:38:36 +0300

    add fucn showMap and saveMap

--- app.py
+++ app.py
@@ -1,17 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 import json
 
 app = Flask(__name__)
 
-with open('lidar/lidar_data.json', 'r', encoding='utf-8') as f:
-    data = json.load(f)
-
-@app.route("/")
+@app.route('/')
 def index():
-    return render_template('index.html', data=data)
+    return render_template('index.html')
 
 
-    
+@app.route('/api/get_data/')
+def get_data():
+    with open('lidar/lidar01.json', 'r', encoding='utf-8') as file:
+        data = json.load(file)
+    return jsonify(data)
 
 
 if __name__ == '__main__':
